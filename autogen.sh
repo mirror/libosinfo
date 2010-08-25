dfccbbdc1a46b8bb2cfb71f8b9b1f6f87e66327e
Daniel P. Berrange <berrange@redhat.com>
2010-08-25 18:04:38 +0100

    Add formal RPM build infrastructure
    * autogen.sh: Rewrite to use gnome-autogen.sh
    * autobuild.sh: Automated RPM build control script
    * configure.ac: Generate RPM specfiles
    * Makefile.am: Distribute RPM specfile templates
    * libosinfo.spec.in, mingw32-libosinfo.spec.in: RPM specs
    * osinfo/Makefile.am, osinfo/libosinfo.syms: Fix Win32
      portability problem

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
-libtoolize
-aclocal
-autoheader
-autoconf --force
-automake --add-missing
-./configure
+# Run this to generate all the initial makefiles, etc.
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+PKG_NAME="libosinfo"
+
+(test -f $srcdir/osinfo/osinfo_db.c) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level $PKG_NAME directory"
+    exit 1
+}
+
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common from the GNOME git"
+    exit 1
+}
+
+ACLOCAL_FLAGS="$ACLOCAL_FLAGS" USE_GNOME2_MACROS=1 . gnome-autogen.sh
