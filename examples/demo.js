2e0120280a2eee32a7097f1b62d4d729c0ded413
Daniel P. Berrange <berrange@redhat.com>
2010-09-02 10:22:09 +0100

    Expand example programs
    Expand the example programs to demonstrate how to obtain the
    the preferred device for an operating system/hypervisor pair
    
    * examples/demo.js, examples/demo.py: Demonstrate device
      filtering

--- examples/demo.js
+++ examples/demo.js
@@ -9,24 +9,43 @@ var db = loader.get_db()
 
 var devs = db.get_device_list()
 
-print (devs)
-
+print ("All device IDs")
 for (idx in devs.get_elements()) {
     var dev = devs.get_nth(idx)
-    print ("Device " + dev.get_id())
+    print ("  Device " + dev.get_id())
 }
 
 var names = db.unique_values_for_property_in_dev("name")
 
+print ("All device names")
 for (idx in names) {
     var name = names[idx]
-    print ("Name " + name)
+    print ("  Name " + name)
 }
 
 var osnames = db.unique_values_for_property_in_os("short-id")
 
+print ("All OS short ids")
 for (idx in osnames) {
     var name = osnames[idx]
 
-    print ("OS short id " + name)
+    print ("  OS short id " + name)
 }
+
+var osid = "http://fedoraproject.org/fedora-11"
+var hvid = "http://qemu.org/qemu-kvm-0.11.0"
+var drvclass = "network"
+//var drvclass = "audio"
+
+var os = db.get_os(osid)
+var hv = db.get_hypervisor(hvid)
+
+var filter = new osinfo.Filter()
+filter.add_constraint("class", drvclass)
+var drvlink = os.get_preferred_device_link(hv, filter)
+var drv = drvlink.get_target()
+print ("For OS '" + os.get_param_value("name") + "' " +
+       "with HV '" + hv.get_param_value("name") + "' " +
+       "for class '" + drvclass + "' " +
+       "use device '" + drv.get_param_value("name") + "' " +
+       "with HV driver '" + drvlink.get_param_value("driver") + "'")
--- examples/demo.py
+++ examples/demo.py
@@ -12,15 +12,36 @@ devs = db.get_device_list()
 
 print devs
 
+print "All device IDs"
 for dev in devs.get_elements():
-    print ("Device " + dev.get_id())
+    print ("  Device " + dev.get_id())
 
 names = db.unique_values_for_property_in_dev("name")
 
+print "All device names"
 for name in names:
-    print ("Name " + name)
+    print ("  Name " + name)
 
 osnames = db.unique_values_for_property_in_os("short-id")
 
+print "All OS short IDs"
 for name in osnames:
-    print ("OS short id " + name)
+    print ("  OS short id " + name)
+
+osid = "http://fedoraproject.org/fedora-11"
+hvid = "http://qemu.org/qemu-kvm-0.11.0"
+drvclass = "network"
+#drvclass = "audio"
+
+os = db.get_os(osid)
+hv = db.get_hypervisor(hvid)
+
+fltr = osinfo.Filter()
+fltr.add_constraint("class", drvclass)
+drvlink = os.get_preferred_device_link(hv, fltr)
+drv = drvlink.get_target()
+print ("For OS '" + os.get_param_value("name") + "' " +
+       "with HV '" + hv.get_param_value("name") + "' " +
+       "for class '" + drvclass + "' " +
+       "use device '" + drv.get_param_value("name") + "' " +
+       "with HV driver '" + drvlink.get_param_value("driver") + "'")
