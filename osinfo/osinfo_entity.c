mirror/libosinfo

commit 84969ff99b26b1aa26c895c01ef091449d7b9e38

entity: Use g_ascii_strtoll to parse int64 strings
osinfo_entity_get_param_value_int64 uses g_ascii_strod to parse
strings to int64, better to use g_ascii_strtoll which is there
for that purpose.

osinfo/osinfo_entity.c
@@ -371,7 +371,7 @@ gint64 osinfo_entity_get_param_value_int64(OsinfoEntity *entity,
     if (str == NULL)
         return -1;
 
-    return (gint64) g_ascii_strtod(str, NULL);
+    return (gint64) g_ascii_strtoll(str, NULL);
 }
 
 gint64 osinfo_entity_get_param_value_int64_with_default(OsinfoEntity *entity,

commit 14defe8e3d2e9f4afd24df6a47cb3a8119a150df

Fix gint64 format string in osinfo_entity_set_param_int64
osinfo_entity_set_param_int64 printed gint64 values using
G_GUINT64_FORMAT which is unwanted when the value is
negative. This bug caused breakage after the changes in
84969ff9 and 4e86e2bf when trying to read the install-reboot value.

Until these commits, we stored the textual representation
of (guint64)-1 (GUINT_MAX) in the entity store, and when trying to convert
it, g_ascii_strtod was returning -GINT64_MAX, and
osinfo_entity_get_param_value_int64_with_default was returning the
default value as this value is negative.

After these commits, g_ascii_strtoll returns GINT64_MAX-1 when trying
to parse the overlong string stored in the database, and the default
value is not returned as the data was found in the database.

Just using the right format (G_GINT64_FORMAT) to convert a gint64 to a
string fixes this bug.

osinfo/osinfo_entity.c
@@ -206,7 +206,7 @@ void osinfo_entity_set_param_int64(OsinfoEntity *entity, const gchar *key, gint6
 {
     gchar *str;
 
-    str = g_strdup_printf("%"G_GUINT64_FORMAT, value);
+    str = g_strdup_printf("%"G_GINT64_FORMAT, value);
     osinfo_entity_set_param(entity, key, str);
     g_free(str);
 }