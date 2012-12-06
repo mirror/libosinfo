37f2f75c344b04d43e095c67cf2389c5aa7395ac
Christophe Fergeau <cfergeau@redhat.com>
2012-12-06 19:04:05 +0100

    Add Fabiano's copyright to some source files
    Fabiano made significant work in the installer code, this commit
    adds his copyright to the files where he is listed in the 'Authors'
    section, this makes things clearer.

--- osinfo/osinfo_install_config_param.c
+++ osinfo/osinfo_install_config_param.c
@@ -2,6 +2,7 @@
  * libosinfo:
  *
  * Copyright (C) 2009-2012 Red Hat, Inc.
+ * Copyright (C) 2012 Fabiano Fidêncio
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
